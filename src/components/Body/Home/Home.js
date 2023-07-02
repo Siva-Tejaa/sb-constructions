e0aab46bfd842831ae71a9cda041f9f771cad7b8
Siva-Tejaa <asivateja1999@gmail.com>
2023-07-02 17:17:23 +0530

    first commit

--- src/components/Body/Home/Home.js
+++ src/components/Body/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
     <div className='bg-[url(https://raw.githubusercontent.com/Siva-Tejaa/Projects-Data/main/SBConstructions%20Background%20Image.png)] h-[25rem] bg-cover bg-center flex flex-col items-center justify-center p-5 text-white gap-2 desktop:h-[30rem]' id="home">
       {/* <img src="https://www.propacademy.co.za/v1/wp-content/uploads/Real-Estate-Courses-South-Africa-2.jpg" className='w-[100%]'/> */}
 
-            <p className='text-[0.8rem] tablet:text-[0.9rem]'>Way In Building & Civil Construction</p>
+            <p className='text-[0.8rem] tablet:text-[0.9rem]'>Way In Building & Civil Construction</p> 
             <p className='font-extrabold text-3xl text-center border-solid tablet:text-4xl'>BUILD YOUR <span className='text-[#ffc413]'>DREAM</span></p>
             <div>
             <p className='text-[0.8rem] text-center tablet:text-[0.9rem]'>We Specialize in commercial, industrial, public infrastructure, housing</p>
